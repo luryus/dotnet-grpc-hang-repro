01e41933bad0299d210cf18e32daf1676b3296e1
Lauri Koskela <luryus@users.noreply.github.com>
2021-05-18 21:07:22 +0300

    Remove unused code

--- Program.cs
+++ Program.cs
@@ -9,13 +9,12 @@ namespace GrpcHang
         static async Task Main(string[] args)
         {
             Console.Write("Testing ({0})...", DateTimeOffset.UtcNow);
-            
+
             StartGcThread();
-            //StartMemPressureThread();
 
             var caller = new RpcCaller();
             await caller.MakeCalls();
-            
+
             Console.WriteLine("No freeze");
             System.Environment.Exit(1);
         }
@@ -27,18 +26,6 @@ namespace GrpcHang
                 while (true)
                 {
                     System.GC.Collect(2);
-                    Thread.Sleep(2);
-                }
-            }).Start();
-        }
-
-        static void StartMemPressureThread()
-        {
-            new Thread(() =>
-            {
-                while (true)
-                {
-                    var b = new byte[1000];
                     Thread.Sleep(2);
                 }
             }).Start();
